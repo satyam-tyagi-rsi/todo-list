1a9e99ee851c0c7ad29f8edcaaf83e51a54bbed6
satyam-tyagi-rsi <satyam.tyagi@rsystems.com>
2019-08-23 17:31:55 +0530

    Added ethereum testing network deploye state

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -23,7 +23,7 @@ contract TodoList {
   );
 
   constructor() public {
-    createTask("Check out dappuniversity.com");
+    createTask("Default to do item");
   }
 
   function createTask(string memory _content) public {
